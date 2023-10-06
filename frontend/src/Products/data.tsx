e532ae5a5fed4a617ef600962d67360bc79ae92e
Nikhil Mummaneni <your_email@example.com>
2023-10-06 18:26:47 -0400

    product-catalog

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,19 @@
+export default {
+    products: [{
+        name: 'shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating :4.5,
+        numberOfReviews:10,
+
+    },
+    {
+        name: 'shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating :4.5,
+        numberOfReviews:10,
+    }]
+]
