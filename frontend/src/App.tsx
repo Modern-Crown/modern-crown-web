ada64689817f728fa18f32b45c66386d54cfc59e
Noah Mendoza <mendoza.841@buckeyemail.osu.edu>
2023-10-10 18:44:02 -0400

    modified:   frontend/src/App.tsx 	modified:   frontend/src/Orders/Orders.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./Home/Home";
 import Products from "./Products/Products";
+import Orders from './Orders/Orders'
 
 function App() {
   
@@ -28,6 +29,7 @@ function App() {
                 <div className="header-links">
                     <Link to="/">Home</Link>
                     <Link to="/catalog">Catalog</Link>
+                    <Link to="/orders">Orders</Link>
                 </div>
             </header>
             <div>
@@ -48,6 +50,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/catalog" element ={<Products />} />
+                    <Route path="orders" element ={<Orders />}></Route>
                 </Routes>
             </main>
         </div>
