c0a92420842e639359f343e960363d62463a0cea
Sydney Armstrong <sydneyarmstrong02@gmail.com>
2023-11-30 12:53:42 -0500

    Lab6 pt 1 - npm start did not work... 	modified:   frontend/src/Products/Products.tsx

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,14 +1,41 @@
-import data from "./data";
+//import data from "./data";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string; 
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageURL: string; 
+}
+const emptyProducts: Product[] = [];
 function Products() {
+    const [products, setProducts] : [Product[], (products: Product[])=>void] = useState(emptyProducts);
+
+    useEffect(()=>{
+        axios.get<Product[]>("https://localhost:7250/catalog",
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response)=> setProducts(response.data))
+        .catch((error)=> console.log(error));
+    }, []);
+
     return(
         <div className="content">
             <ul className="product">
-                {data.products.map((Products)=>(
+                {products.map((Products)=>(
                   <li>
                     <div className="product">
                         <img
                         className="product-image"
-                        src={Products.imageUrl}
+                        src={Products.imageURL}
                         alt="product"
                         />
                         <div className = "product-name">
